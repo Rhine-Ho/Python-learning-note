3024aeba7a5cabd990f258a2f9a81bab92b5e75d
River-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2022-11-03 00:08:25 +0800

    Update form.py

--- SQL/form.py
+++ SQL/form.py
@@ -1,9 +1,12 @@
 import csv
 # first"" is your opening file,second is the function of read
 with open("form.csv", "r")as file:
-    reader = csv.reader(file)
+    # reader = csv.reader(file)//reader will show all the list in the output
+    # but if use dictionary will be output colletion of the key value pair.
+    reader = csv.DictReader(file)
     for row in reader:
 
-    favorite = row[1]
+    form = row[1]
+    print(form[1])
     # print(row[1])
 # if just row like this the computer will just show you the hall the string in that line.
