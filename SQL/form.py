edd6fd68b92c542fd85531eb57f50f947aee4917
River-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2022-11-02 23:55:38 +0800

    Update form.py

--- SQL/form.py
+++ SQL/form.py
@@ -5,5 +5,5 @@ with open("form.csv", "r")as file:
     for row in reader:
 
     favorite = row[1]
-    print(row[1])
+    # print(row[1])
 # if just row like this the computer will just show you the hall the string in that line.
