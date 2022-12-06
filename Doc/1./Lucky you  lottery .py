810589e90bf804f8920ed99800afcf4c6029c01c
River-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2022-12-06 23:12:55 +0800

    Update Lucky you  lottery .py

--- Doc/1./Lucky you  lottery .py
+++ Doc/1./Lucky you  lottery .py
@@ -1,2 +1,6 @@
 win = open('winNumber.txt').read().split()
 mine = open('myNumber.txt').read().split()
+
+for i, num in enumerate(mine):
+    if num in win:
+        print('Number', i+1, 'lottery ticket', num, 'is win !')
