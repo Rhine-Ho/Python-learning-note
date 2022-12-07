efbf6c9b94016cd3acc3fc42b9ee5e8213f8e8de
River-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2022-12-07 20:55:26 +0800

    Delete Lucky you  lottery .py

--- Doc/1./Lucky you  lottery .py
+++
@@ -1,8 +0,0 @@
-win = open('winNumber.txt').read().split()
-mine = open('myNumber.txt').read().split()
-
-for i, num in enumerate(mine):
-    if num in win:
-        print('Number', i+1, 'lottery ticket', num, 'is win !')
-    else:
-        print('Sorry try next time.')
